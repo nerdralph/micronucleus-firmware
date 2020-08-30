4ed41887a60ee7c38f2bbca7d99e1ae5d9864923
Nerd Ralph <ralphdoncaster@gmail.com>
2020-08-30 13:55:39 -0300

    dedicate r10 to usbRxLen

--- firmware/usbdrv/usbdrv.c
+++ firmware/usbdrv/usbdrv.c
@@ -36,27 +36,21 @@ uchar usbRxBuf[USB_BUFSIZE];  /* raw RX buffer: PID, 8 bytes data, 2 bytes CRC *
 uchar       usbDeviceAddr;      /* assigned during enumeration, defaults to 0 */
 uchar       usbNewDeviceAddr;   /* device ID which should be set after status phase */
 uchar       usbConfiguration;   /* currently selected configuration. Administered by driver, but not used */
-volatile schar usbRxLen;        /* = 0; number of bytes in usbRxBuf; 0 means free, -1 for flow control */
+//volatile schar usbRxLen;        /* = 0; number of bytes in usbRxBuf; 0 means free, -1 for flow control */
+register volatile schar usbRxLen asm("r10");        /* = 0; usbRxBuf byte count; 0 means free, -1 for flow control */
 uchar       usbCurrentTok;      /* last token received or endpoint number for last OUT token if != 0 */
 uchar       usbRxToken;         /* token for data we received; or endpont number for last OUT */
-#ifdef MNHACK_NO_DATASECTION
-  volatile uchar usbTxLen;   /* number of bytes to transmit with next IN token or handshake token */
-#else
-  volatile uchar usbTxLen = USBPID_NAK;   /* number of bytes to transmit with next IN token or handshake token */
-#endif
+volatile uchar usbTxLen;   /* number of bytes to transmit with next IN token or handshake token */
+//register volatile uchar usbTxLen asm("r17");   /* number of bytes to transmit with next IN token or handshake token */
 uchar       usbTxBuf[USB_BUFSIZE];/* data to transmit with next IN, free if usbTxLen contains handshake token */
 
 //usbMsgPtr_t         usbMsgPtr;      /* data to tx next -- flash address */
 register usbMsgPtr_t         usbMsgPtr asm("r8");      /* data to tx next -- flash address */
 
 /* USB status registers / not shared with asm code */
-#ifdef MNHACK_NO_DATASECTION
 // static usbMsgLen_t  usbMsgLen; /* remaining number of bytes */
 //  usbMsgLen_t  usbMsgLen; /* remaining number of bytes */
 register usbMsgLen_t  usbMsgLen asm("r16"); /* remaining number of bytes */
-#else
-  static usbMsgLen_t  usbMsgLen = USB_NO_MSG; /* remaining number of bytes */
-#endif
 
 #define USB_FLG_USE_USER_RW     (1<<7)
 
